55880fce6eabce2ed9b2403429155edffbc711e5
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-27 15:51:32 +0000

    added best of 3 mode and button styling

--- js/game.js
+++ js/game.js
@@ -14,16 +14,43 @@ function startGame() {
     counter = 0;
     outputMessage("The game has begun!");
     hideStartButton();
+    enableOptions();
 }
 function outputMessage(message) {
     var output = document.querySelector(".output");
     output.innerHTML += "<p>".concat(message, "</p>");
+}
+function clearOutput() {
+    var output = document.querySelector(".output");
+    output.innerHTML = "";
+}
+function clearWinner() {
+    var winnerMsg = document.querySelector(".winner-msg");
+    winnerMsg.innerHTML = "";
 }
 function hideStartButton() {
     var startButton = document.querySelector(".start-button");
     var gameButtons = document.querySelector(".game-buttons");
     startButton.style.display = "none";
     gameButtons.style.display = "block";
+}
+function showStartButton() {
+    var startButton = document.querySelector(".start-button");
+    var gameButtons = document.querySelector(".game-buttons");
+    startButton.style.display = "inline";
+    gameButtons.style.display = "none";
+}
+function enableOptions() {
+    var options = document.querySelectorAll(".option");
+    for (var i = 0; i < options.length; i++) {
+        options[i].disabled = false;
+    }
+}
+function disableOptions() {
+    var options = document.querySelectorAll(".option");
+    for (var i = 0; i < options.length; i++) {
+        options[i].disabled = true;
+    }
 }
 function getComputerMove() {
     var move = Math.floor(Math.random() * 3);
@@ -38,9 +65,9 @@ function handleUserChoice(choice) {
         player: "User",
     };
     var computerGuess = getComputerMove();
+    var winner = calculateWinner(userGuess, computerGuess);
     outputMessage("You chose ".concat(moves[userGuess.move]));
     outputMessage("Computer chose ".concat(moves[computerGuess.move]));
-    var winner = calculateWinner(userGuess, computerGuess);
     if (winner.player === "User") {
         userScore++;
     }
@@ -48,6 +75,7 @@ function handleUserChoice(choice) {
         computerScore++;
     }
     outputMessage("".concat(winner.player, " wins with ").concat(moves[winner.move]));
+    checkProgress();
 }
 function calculateWinner(guessOne, guessTwo) {
     if (guessOne.move == guessTwo.move)
@@ -72,6 +100,27 @@ function calculateWinner(guessOne, guessTwo) {
             return guessOne;
     }
     return guessOne;
+}
+function checkProgress() {
+    var resetButton = document.querySelector(".reset-button");
+    var winnerMsg = document.querySelector(".winner-msg");
+    if (userScore === 2) {
+        winnerMsg.innerHTML = "<p>User wins best of three!</p>";
+        resetButton.style.display = "inline";
+        disableOptions();
+    }
+    else if (computerScore === 2) {
+        winnerMsg.innerHTML = "<p>Computer wins best of three</p>";
+        resetButton.style.display = "inline";
+        disableOptions();
+    }
+}
+function reset() {
+    clearOutput();
+    clearWinner();
+    showStartButton();
+    var resetButton = document.querySelector(".reset-button");
+    resetButton.style.display = "none";
 }
 // let computerGuess: guess = getComputerMove();
 // console.log(computerGuess);
--- ts/game.ts
+++ ts/game.ts
@@ -15,6 +15,7 @@ function startGame(): void {
   counter = 0;
   outputMessage("The game has begun!");
   hideStartButton();
+  enableOptions();
 }
 
 function outputMessage(message: string): void {
@@ -22,11 +23,44 @@ function outputMessage(message: string): void {
   output.innerHTML += `<p>${message}</p>`;
 }
 
+function clearOutput(): void {
+  let output: HTMLDivElement = document.querySelector(".output");
+  output.innerHTML = ``;
+}
+
+function clearWinner(): void {
+  let winnerMsg: HTMLDivElement = document.querySelector(".winner-msg");
+  winnerMsg.innerHTML = ``;
+}
+
 function hideStartButton(): void {
   let startButton: HTMLButtonElement = document.querySelector(".start-button");
   let gameButtons: HTMLDivElement = document.querySelector(".game-buttons");
   startButton.style.display = "none";
   gameButtons.style.display = "block";
+}
+
+function showStartButton(): void {
+  let startButton: HTMLButtonElement = document.querySelector(".start-button");
+  let gameButtons: HTMLDivElement = document.querySelector(".game-buttons");
+  startButton.style.display = "inline";
+  gameButtons.style.display = "none";
+}
+
+function enableOptions(): void {
+  let options: NodeListOf<HTMLButtonElement> =
+    document.querySelectorAll(".option");
+  for (let i = 0; i < options.length; i++) {
+    options[i].disabled = false;
+  }
+}
+
+function disableOptions(): void {
+  let options: NodeListOf<HTMLButtonElement> =
+    document.querySelectorAll(".option");
+  for (let i = 0; i < options.length; i++) {
+    options[i].disabled = true;
+  }
 }
 
 interface guess {
@@ -48,9 +82,9 @@ function handleUserChoice(choice: number): void {
     player: "User",
   };
   let computerGuess: guess = getComputerMove();
+  let winner: guess = calculateWinner(userGuess, computerGuess);
   outputMessage(`You chose ${moves[userGuess.move]}`);
   outputMessage(`Computer chose ${moves[computerGuess.move]}`);
-  let winner: guess = calculateWinner(userGuess, computerGuess);
   if (winner.player === "User") {
     userScore++;
   }
@@ -58,6 +92,7 @@ function handleUserChoice(choice: number): void {
     computerScore++;
   }
   outputMessage(`${winner.player} wins with ${moves[winner.move]}`);
+  checkProgress();
 }
 
 function calculateWinner(guessOne: guess, guessTwo: guess): guess {
@@ -86,5 +121,26 @@ function calculateWinner(guessOne: guess, guessTwo: guess): guess {
   return guessOne;
 }
 
+function checkProgress(): void {
+  let resetButton: HTMLDivElement = document.querySelector(".reset-button");
+  let winnerMsg: HTMLDivElement = document.querySelector(".winner-msg");
+  if (userScore === 2) {
+    winnerMsg.innerHTML = `<p>User wins best of three!</p>`;
+    resetButton.style.display = "inline";
+    disableOptions();
+  } else if (computerScore === 2) {
+    winnerMsg.innerHTML = `<p>Computer wins best of three</p>`;
+    resetButton.style.display = "inline";
+    disableOptions();
+  }
+}
+
+function reset(): void {
+  clearOutput();
+  clearWinner();
+  showStartButton();
+  let resetButton: HTMLDivElement = document.querySelector(".reset-button");
+  resetButton.style.display = "none";
+}
 // let computerGuess: guess = getComputerMove();
 // console.log(computerGuess);
